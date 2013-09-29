4649ffb4368222a9bfd349892ed043df536df556
hellboy1975 <hellboy1975@gmail.com>
2013-09-29 23:01:52 +0930

    CKEditor added

---
+++ web/js/aaol.js
@@ -0,0 +1,27 @@
+//aaol.js
+
+// IIFE - Immediately Invoked Function Expression
+  (function(yourcode) {
+
+    // The global jQuery object is passed as a parameter
+  	yourcode(window.jQuery, window, document);
+
+  }(function($, window, document) {
+
+    // The $ is now locally scoped 
+
+   // Listen for the jQuery ready event on the document
+   $(function() {
+
+     // The DOM is ready!
+
+     $( 'textarea.html-editor' ).ckeditor();
+
+   });
+
+   // The rest of the code goes here!
+
+  
+  }));
+
+
