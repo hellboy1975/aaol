fef2584540bc3d968ba0bac007c9c8249c6e655f
hellboy1975 <hellboy1975@gmail.com>
2013-10-12 16:12:20 +1030

    Added some defs use for PostModel

--- src/config.php
+++ src/config.php
@@ -2,5 +2,20 @@
 
 // Constant definitions.
 
-define("SQL_PASSWORD", "somethingelse");
+define("SQL_PASSWORD", "notthepassword");
+
+// posts model constants
+
+define("POST_TYPE_NEWS", "NEWS");
+define("POST_TYPE_USER", "USER");
+define("POST_TYPE_DEV", "DEV");
+define("POST_TYPE_ALL", "ALL");
+
+define("POST_ORDER_DATE_ASC", "DATE_ASC");
+define("POST_ORDER_DATE_DESC", "DATE_DESC");
+define("POST_ORDER_ID_DESC", "ID_DESC");
+
+define("DEBUG_APP", true);
+
+
 
