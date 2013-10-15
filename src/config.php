a3699a7ea2cdb8bdc6c3bf684ac28f8476f32909
hellboy1975 <hellboy1975@gmail.com>
2013-10-15 21:43:44 +1030

    Added some new Defines for USER types

--- src/config.php
+++ src/config.php
@@ -2,18 +2,29 @@
 
 // Constant definitions.
 
-define("SQL_PASSWORD", "notthepassword");
+define("SQL_PASSWORD", "nope");
+
+ define("TYPE_ALL", "ALL");
 
 // posts model constants
-
 define("POST_TYPE_NEWS", "NEWS");
 define("POST_TYPE_USER", "USER");
 define("POST_TYPE_DEV", "DEV");
 define("POST_TYPE_ALL", "ALL");
 
+define("USER_STATUS_ACTIVE", 'A');
+define("USER_STATUS_INACTIVE", 'I');
+define("USER_STATUS_REG_PENDING", 'R');
+
+define('USER_ROLE_ADMIN', 'ROLE_ADMIN');
+define('USER_ROLE_USER', 'ROLE_USER');
+
 define("POST_ORDER_DATE_ASC", "DATE_ASC");
 define("POST_ORDER_DATE_DESC", "DATE_DESC");
 define("POST_ORDER_ID_DESC", "ID_DESC");
+
+define("USER_ORDER_USERNAME_ASC", "UN_ASC");
+define("USER_ORDER_USERNAME_DESC", "UN_DESC");
 
 define("DEBUG_APP", true);
 
